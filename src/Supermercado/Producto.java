8e74826d97b43b62ca6d901513cd5f3ec150cac9
Ayelen Guadalupe Aguero <agueroayelen10@gmail.com>
2025-09-05 19:43:37 -0300

    Add files via upload

---
+++ src/Supermercado/Producto.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Supermercado;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+     private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String rubro; 
+
+    public Producto(int codigo, String descripcion, double precio, int stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        Rubro(rubro);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+    
+    public void Rubro(String rubro){
+        if (rubro.equalsIgnoreCase("comestible")||rubro.equalsIgnoreCase("perfumeria")||rubro.equalsIgnoreCase("limpieza")) {
+            this.rubro=rubro;
+        }else{
+            System.out.println("¡Rubro invalido!");
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + this.codigo;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.precio) != Double.doubleToLongBits(other.precio)) {
+            return false;
+        }
+        if (this.stock != other.stock) {
+            return false;
+        }
+        if (!Objects.equals(this.descripcion, other.descripcion)) {
+            return false;
+        }
+        if (!Objects.equals(this.rubro, other.rubro)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", precio=" + precio + ", stock=" + stock + ", rubro=" + rubro + '}';
+    }
+}
